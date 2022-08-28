9c347b838b3cbeddb4b70605b4de441d3ee4ca37
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-08-28 08:21:16 -0300

    Função lambda

---
+++ Aula5/Teorica/ex4.py
@@ -0,0 +1,14 @@
+"""
+--------Função lambda------
+-Funções mais simples, sem nome, chamada de funções lambda
+-Elas podem ser escritas em uma só linha de código e dentro
+do programa principal
+"""
+
+#Exemplo
+res = lambda x: x * x
+print(res(3))
+
+#Exemplo 2
+soma = lambda x, y: x + y
+print(soma(3, 5))
