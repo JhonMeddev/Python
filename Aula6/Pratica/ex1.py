10664cc3c901b768da193a6ec1b4b1d4b3784738
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-09-01 11:11:00 -0300

    Importando bibliotecas python

---
+++ Aula6/Pratica/ex1.py
@@ -0,0 +1,6 @@
+import math
+# nomear nome da biblioteca > import math as m
+# importar somente a que utilizar > from math import sqrt
+# biblioteca python https://docs.python.org/3/library/index.html
+
+print(math.sqrt(9))
